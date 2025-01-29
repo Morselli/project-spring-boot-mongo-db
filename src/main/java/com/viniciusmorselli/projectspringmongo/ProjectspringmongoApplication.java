d830a2f4334e6671a6f210a0f5418ce987784d02
Morselli <vinicius_morselli@hotmail.com>
2025-01-28 23:28:27 -0300

    First commit - created project

---
+++ src/main/java/com/viniciusmorselli/projectspringmongo/ProjectspringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusmorselli.projectspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectspringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectspringmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusmorselli/projectspringmongo/ProjectspringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusmorselli.projectspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
