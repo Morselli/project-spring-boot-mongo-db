7a41a296e6cc38cf2b9be224ed83b08debc22576
Morselli <vinicius_morselli@hotmail.com>
2025-02-02 19:12:11 -0300

    Add comments to posts

--- src/main/java/com/viniciusmorselli/projectspringmongo/domain/Post.java
+++ src/main/java/com/viniciusmorselli/projectspringmongo/domain/Post.java
@@ -2,11 +2,14 @@ package com.viniciusmorselli.projectspringmongo.domain;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.viniciusmorselli.projectspringmongo.dto.AuthorDTO;
+import com.viniciusmorselli.projectspringmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
   private String title;
   private String body;
   private AuthorDTO author;
+
+  private List<CommentDTO> comments = new ArrayList<>();
 
   public Post() {
   }
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
   public void setAuthor(AuthorDTO author) {
     this.author = author;
+  }
+
+  public List<CommentDTO> getComments() {
+    return comments;
+  }
+
+  public void setComments(List<CommentDTO> comments) {
+    this.comments = comments;
   }
 
   @Override
