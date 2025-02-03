ca8d5ed3eb65e36ff6253e67cd256128ecfa7215
Morselli <vinicius_morselli@hotmail.com>
2025-02-02 21:28:04 -0300

    Simple query with query methods

--- src/main/java/com/viniciusmorselli/projectspringmongo/resources/PostResource.java
+++ src/main/java/com/viniciusmorselli/projectspringmongo/resources/PostResource.java
@@ -1,12 +1,17 @@
 package com.viniciusmorselli.projectspringmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.viniciusmorselli.projectspringmongo.domain.Post;
+import com.viniciusmorselli.projectspringmongo.resources.utils.URL;
 import com.viniciusmorselli.projectspringmongo.services.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -21,4 +26,12 @@ public class PostResource {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
 	}
+
+	@GetMapping(value = "/titleSearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
